45270230fbc1bcc7de9605b91f5b9c1894c60640
Adam Tannir <atannir@gmail.com>
2014-08-11 09:59:52 -0400

    late
    first attempt, for learning purposes only at this point

---
+++ plot1.R
@@ -0,0 +1,35 @@
+## plot1.R
+##
+## Adam Tannir
+## Coursera Exploratory Data Analysis
+## Assignment 1, part 1
+##
+##
+## household power consumption for 2 days, 2007-02-01 and 2007-02-02.
+##
+## zip is 20 MB, full txt is 127 MB.
+##
+## Suggested to read only the data for those dates, but still researching how.
+## 15 seconds to read whole dataset on my machine, 21 without column pre-specified
+## using the offsets, load time is under 1 second
+##
+## hints for large files from http://www.biostat.jhsph.edu/~rpeng/docs/R-large-tables.html
+##
+## strptime(paste(hpc_sample[[1]][1],hpc_sample[[2]][1], sep=" " ), format = "%d/%m/%Y %H:%M:%S")
+##
+##
+
+xwidth <- 480
+ywidth <- 480
+
+
+hpc_sample <- read.table("household_power_consumption.txt", sep=";",header = TRUE, na.strings="?", nrows = 10)
+classes <- sapply(hpc_sample, class)
+classes["Date"] <- "character"
+classes["Time"] <- "character"
+## tried to be fancy with dates ahead of time, didn't work.
+## offsets found using grep -n on command line
+hpc <- read.table("household_power_consumption.txt", colClasses = classes, sep = ";", header = TRUE, na.strings="?", skip = 66636, nrows = 2880)
+colnames(hpc) <- colnames(hpc_sample)
+## rm(hpc_sample)
+
